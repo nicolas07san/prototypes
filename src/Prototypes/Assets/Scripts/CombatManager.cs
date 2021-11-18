edcc1e7b05dd7d1039c472f3f5632396f9564dee
nicolas07san <nicolas07san@gmail.com>
2021-11-18 15:38:09 -0300

    Round Start Animation Finished

--- src/Prototypes/Assets/Scripts/CombatManager.cs
+++ src/Prototypes/Assets/Scripts/CombatManager.cs
@@ -6,10 +6,16 @@ public class CombatManager : MonoBehaviour
 {
     [SerializeField] private GameObject playerHand;
     [SerializeField] private GameObject enemyHand;
-    [SerializeField] private GameObject dice;
+
+    [SerializeField] private TMPro.TMP_Text diceText;
+    [SerializeField] private TMPro.TMP_Text roundText;
 
     private Vector3 playerHandPosition = new Vector3(-600, 0);
     private Vector3 enemyHandPosition = new Vector3(600, 0);
+
+    private int randomNumber;
+
+    private int round = 1;
 
     void Start()
     {
@@ -18,6 +24,9 @@ public class CombatManager : MonoBehaviour
 
         InitialCardPlacement(playerHand);
         InitialCardPlacement(enemyHand);
+
+        StartCoroutine(StartRoundAnimation());
+        
     }
     
     private void InitialCardPlacement(GameObject cardHand)
@@ -46,6 +55,43 @@ public class CombatManager : MonoBehaviour
 
             card.SetActive(true);
 
+        }
+    }
+
+    public IEnumerator StartRoundAnimation() {
+        Debug.Log("Coroutine Iniciada");
+        Vector3 startPosition = new Vector3(0, -200);
+        Vector3 finalPosition = new Vector3(0, 200);
+
+        roundText.gameObject.SetActive(true);
+        roundText.transform.localPosition = startPosition;
+        roundText.text = "Round " + round;
+        
+
+        while(Vector3.Distance(roundText.transform.localPosition, finalPosition) > 0.05f)
+        {
+            roundText.transform.localPosition = Vector3.Lerp(roundText.transform.localPosition, finalPosition, 0.0125f);
+            Debug.Log("Dentro do While");
+            yield return null;
+        }
+
+        roundText.transform.localPosition = finalPosition;
+        roundText.gameObject.SetActive(false);
+
+        StartCoroutine(RollDice());
+
+        yield return null;
+
+    }
+
+    public IEnumerator RollDice()
+    {
+
+        for (int i = 0; i < 100; i++)
+        {
+            randomNumber = Random.Range(1, 6);
+            diceText.text = randomNumber.ToString();
+            yield return new WaitForSecondsRealtime(3f / 100);
         }
     }
 }
