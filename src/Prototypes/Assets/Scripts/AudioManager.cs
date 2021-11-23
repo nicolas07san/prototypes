6513c29d95048c3475569d33e29f7d87f8fbd5da
nicolas07san <nicolas07san@gmail.com>
2021-11-23 16:58:03 -0300

    AudioManager added but not finished

---
+++ src/Prototypes/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
