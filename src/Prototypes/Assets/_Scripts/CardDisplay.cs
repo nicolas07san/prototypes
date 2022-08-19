507c9f4205a7409eaf92a561d6a3c2f88684e9b1
Nicorovisk <nicolas07san@gmail.com>
2022-08-18 22:02:13 -0300

    card visual finish

--- src/Prototypes/Assets/_Scripts/CardDisplay.cs
+++ src/Prototypes/Assets/_Scripts/CardDisplay.cs
@@ -24,11 +24,13 @@ public class CardDisplay : MonoBehaviour
     [SerializeField] private TMP_Text _supportActionCostText;
     [SerializeField] private TMP_Text _supportActionAmountText;
     [SerializeField] private Button _supportActionButton;
+    [SerializeField] private GameObject _supportActionShieldIcon;
+    [SerializeField] private GameObject _supportActionHealthIcon;
 
     [Header("Special Attack")]
     [SerializeField] private TMP_Text _specialAttackDmgText;
     [SerializeField] private Button _specialAttackButton;
-
+    [SerializeField] private GameObject _specialAttackCombo;
 
     [Header("Base Stats")]
     [SerializeField] private TMP_Text _healthText;
@@ -63,9 +65,31 @@ public class CardDisplay : MonoBehaviour
         // Support Action
         _supportActionCostText.text = _card.supportActionCost.ToString();
         _supportActionAmountText.text = _card.supportActionAmount.ToString();
+        if (_card.isShield)
+            _supportActionShieldIcon.SetActive(true);
+        else
+            _supportActionHealthIcon.SetActive(true);
 
         // Special Attack
         _specialAttackDmgText.text = _card.specialAttackDmg.ToString();
+        for(int i = 0; i < _card.combo.Length; i++)
+        {
+            switch (_card.combo[i])
+            {
+                case (Card.Action.LightAttack):
+                    Instantiate(_lightAttackIcon, _specialAttackCombo.transform);
+                    break;
+                case (Card.Action.HeavyAttack):
+                    Instantiate(_heavyAttackIcon, _specialAttackCombo.transform);
+                    break;
+                case (Card.Action.SupportAction):
+                    if(_card.isShield)
+                        Instantiate(_shieldIcon, _specialAttackCombo.transform);
+                    else
+                        Instantiate(_healthIcon, _specialAttackCombo.transform);
+                    break;
+            }
+        }
 
         //Base stats
         _healthText.text = _card.health.ToString();
